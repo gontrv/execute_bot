2054ee106942d142c3b3abfa01c42955ffc485b9
Oleksandr <gontrv@gmail.com>
2017-01-21 15:32:28 +0200

    добавлены все файлы проекта

---
+++ ExecuteBot/BotExecuter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChangerProxy;
+using System.IO;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Net;
+using ExplorerByChrome;
+
+namespace ExecuteBot
+{
+    class BotExecuter
+    {
+        const string PATH_TO_RAW_PROXY = @".\raw_proxy.txt";
+        List<string> _rawProxyList = null;
+        ChangerProxy.ChangerProxy changerProxy = new ChangerProxy.ChangerProxy();
+        int _executeNumber;
+        string _currentProxy = "";
+        Process _chrome = null;
+
+        public BotExecuter(int countOfProxyToExecute)
+        {
+            _executeNumber = countOfProxyToExecute;
+        }
+
+        public void ExecuteBot()
+        {
+            if (FillRawProxyList())
+            {
+                for (int i = 0; i < _executeNumber; i++)
+                {
+                    _currentProxy = _rawProxyList[i];
+                    if (changerProxy.IsProxyValid(_currentProxy))
+                    {
+                        changerProxy.ChangeProxySettings(_currentProxy);
+                        _chrome = Process.Start("Chrome.exe");
+                        _chrome.WaitForExit();
+                        DeleteChromeData();
+                    }
+                }
+            }
+        }
+
+        public bool FillRawProxyList()
+        {
+            if (File.Exists(PATH_TO_RAW_PROXY))
+            {
+                _rawProxyList = new List<string>(File.ReadAllLines(PATH_TO_RAW_PROXY));
+                return true;
+            }
+            return false;
+        }
+
+        private void DeleteChromeData()
+        {
+
+        }
+    }
+}
---
+++ ExecuteBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Numbers for execution: ");
+                int count = int.Parse(Console.ReadLine());
+
+                BotExecuter botExecuter = new BotExecuter(count);
+                Console.WriteLine("Press enter to execute...");
+                Console.ReadLine();
+                botExecuter.ExecuteBot();
+
+            } while (true);
+        }
+    }
+}
